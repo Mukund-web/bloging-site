b4d1816161918b5a46a78f95c97dae3394dc1479
Mukund <mukundclg310@gmail.com>
2026-06-21 07:22:11 +0530

    prodect start from this point

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className = "flex items-center justify-center h-screen w-full bg-fuchsia-900">
+      <h1 className = " text-white text-4xl font-bold">BLOG site with appwrite</h1>
+    </div>
+  )
+}
+
+export default App
